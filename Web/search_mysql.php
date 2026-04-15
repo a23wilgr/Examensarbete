9900c14a514c4685456efaa911fedde1f7784f11
a23wilgr <a23wilgr@student.his.se>
2026-04-15 20:19:10 +0200

    Connection till MySQL

--- Web/search_mysql.php
+++ Web/search_mysql.php
@@ -1,4 +1,18 @@
 <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "root123";
+    $db_name = "articles_32k_mysql";
 
+    try {
+        $pdo = new PDO('mysql:host='.$servername.';dbname='.$db_name, $username, $password);
 
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        echo "Connection OK";
+
+    }
+    catch(PDOException $error) {
+        echo "Connection failed: " . $error->getMessage();
+    }
 ?>
